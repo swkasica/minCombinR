c7db2c30f8379878ff4d7d21c9be4e45fbe9fcba
sfisher <shannah.elizabeth@gmail.com>
2018-06-07 10:15:10 -0700

    change plot_heatmap, including facetting

--- R/common_statistical_fcns.R
+++ R/common_statistical_fcns.R
@@ -36,11 +36,11 @@ plot_stacked_bar_chart <- function(data, x, fill, facet_by) {
 
 # Line Chart
 # Note  - added group features to resolve an error
-plot_line_chart <- function(data, x, y, group,facet_by) {
+plot_line_chart <- function(data, x, y, group, facet_by) {
   if(missing(group)){
-    gg_chart <- ggplot(data, aes_string(x, y,group=1)) + geom_line()
+    gg_chart <- ggplot(data, aes_string(x = x, y = y, group = 1)) + geom_line()
   }else{
-    gg_chart <- ggplot(data, aes_string(x=x, y=y,group=group)) + geom_line(aes_string(colour = group))
+    gg_chart <- ggplot(data, aes_string(x = x, y = y, group = group)) + geom_line(aes_string(colour = group))
   }
 
   if (!missing(facet_by)) {
@@ -50,56 +50,30 @@ plot_line_chart <- function(data, x, y, group,facet_by) {
 }
 
 # Heatmap
-# create_hm is a helper function for plot_heatmap...
-# can place in plot_heatmap function but would like to have it out for now otherwise it's too confusing
-# Note - would be good to discuss further, this is a note quite what most people need in a heatmap. Made modes
-
-#@param x data frame for plotting
-#@param facet logical
-create_hm <- function(dat, x, y,fill) {
-  # if (!missing(x) && !missing(y)) {
-  #   data_as_df <- dat %>% dplyr::group_by_(x, y) %>% dplyr::tally()
-  #   data_as_mat <- reshape2::dcast(data_as_df, paste(y, "~", x), value.var="n")
-  #   data_as_mat[is.na(data_as_mat)] <- 0
-  #   rownames(data_as_mat) <- data_as_mat[[y]]
-  #   dat <- data_as_mat %>% dplyr::select(-y)
-  # }
-
-  tmp<-data.matrix(dat,rownames.force = TRUE)
-
-  hm <- pheatmap::pheatmap(
-    mat               = dat,
-    cluster_rows      = FALSE,
-    cluster_cols      = FALSE,
-    fontsize_row      = 8,
-    fontsize_col      = 8,
-    main              = "Heatmap Title")
-  hm$gtable
-}
-
-#' Plot a heatmap
-#' @param data A data frame
-#' @param x optional categorical variable for x-axis if quantitative value attribute has not been calculated in data
-#' @param y optional categorical variable for y-axis. If given (along with x_col), will calculate the number of occurences with x_var.
-plot_heatmap <- function(data, x, y, facet_by){
-  if(missing(x) && !missing(y) || !missing(x) && missing(y)) {
-    stop("You must input BOTH or NEITHER a x_var and y_var.")
-  }
-  if (missing(x) && !missing(facet_by)){
-    stop("The feature facet_by has not been implemented for a matrix input yet!!!")
+# Heatmap
+# In this function, I am assuming the data input is a matrix with numeric values in the cells and a column for facetting
+plot_heatmap <- function(data, facet_by) {
+  create_hm <- function(dat, title) {
+    pheatmap::pheatmap(
+      mat               = dat,
+      cluster_rows      = FALSE,
+      cluster_cols      = FALSE,
+      fontsize_row      = 8,
+      fontsize_col      = 8,
+      main              = title)
+    # hm$gtable
   }
 
-  if(missing(facet_by)) {
-    if(missing(x) && missing(y)) {
-      create_hm(data)
-    } else {
-      create_hm(data,x,y)
-    }
+  if (!missing(facet_by)) {
+    facet_dat <- lapply(unique(data[[facet_by]]),
+                        function(x) {dplyr::filter_(data, paste(facet_by, "==", quote(x)))})
+    all_plots <- lapply(facet_dat,
+                        function(x) create_hm(select(x, -one_of(facet_by)), unique(x[[facet_by]])))
   } else {
-    facet_dat <- lapply(unique(data[[facet_by]]), function(x) {dplyr::filter(data, group == x)})
-    all_plots <- lapply(facet_dat, FUN=create_hm, x, y)
-    combine_many_types_general(all_plots)
+    all_plots <- list(create_hm(data))
   }
+  return(all_plots)
+
 }
 
 # Divergent Bar chart
