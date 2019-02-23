a5f842b2065fe1e515f469f6e7aa280ba7464e23
amcrisan <anamaria.crisan@bccdc.ca>
2019-02-22 16:30:19 -0800

    add spatial join

--- R/data_input_fcn.R
+++ R/data_input_fcn.R
@@ -281,20 +281,41 @@ join_spatial_data<-function(...,obj_names = NULL){
     if(obj@type !="spatial") next
 
     geo_tmp<-obj@data$geometry
-    geo_meta_tmp<-obj@data$metadata
+    geo_meta_tmp<-if(!is.null(obj@data$metadata)) obj@data$metadata else NULL
 
-    item_id<-if(is.null(obj_name)) obj@id else obj_names[idx]
+    item_id<-if(is.null(obj_names)) obj@id else obj_names[idx]
 
+    ##DEV NOTE: Assumes shape file only has geometry column
+    # When read in as a shape file
     #adding to the geometry item
     geo_tmp_col<-colnames(geo_tmp)
+
+    if(length(geo_tmp_col)>1){
+      warning("This method assumes that your shape file data only contains geometry information. Strange sideffects may ensue.")
+    }
+
     geo_tmp<-cbind(rep(item_id,times = nrow(geo_tmp)),geo_tmp)
-    colnames(geo_tmp_col)<- c()
+    colnames(geo_tmp)<- c("minID",geo_tmp_col)
+
+    geo_data<-rbind(geo_data,geo_tmp)
 
     #adding to metadata item
+    if(is.null(geo_meta_tmp) | all(is.na(geo_meta_tmp))) next
 
+    geo_meta_col<-colnames(geo_meta_tmp)
+    geo_meta_tmp<-cbind(rep(item_id,times = nrow(geo_tmp)),geo_meta_tmp)
+    colnames(geo_meta_tmp)<-c("minID",geo_tmp_col)
 
+    geo_metadata<-rbind(geo_metadata,geo_meta_tmp)
   }
 
+  objDat<-new("gevitDataObj",
+              id  = paste("spatial",dataID,sep="_"),
+              type = "spatial",
+              source = paste(names(spatial_obj),sep=";"),
+              data = list(geometry=geo_data,
+                          metadata = geo_metadata))
+  return(objDat)
 }
 
 
